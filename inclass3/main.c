01f2755957170211eb187eaf0a16b68a90610fcf
gsahinpi <gsahinpi@gmail.com>
2022-09-30 21:57:06 +0300

    in class examples

--- inclass3/main.c
+++ inclass3/main.c
@@ -1,25 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+float foo(int [],int size);
 int main()
-{
-    int ar[]={1,2,3};
-    int i;
-    int dummy;
-    for (i=0;i<3;i++)
-    {
-      dummy=ar[i] ;
-      printf( "%d\n",dummy  ) ;
+{int i;
 
-
-    }
-    for (i=0;i<3;i++)
-    {
-      dummy=*(ar+i) ;
-      printf( "%d\n",dummy  ) ;
-
-
-    }
     printf("Hello world!\n");
+
+    int ar[]={1,2,3};
+    int * aptr;
+    aptr=ar;
+
+    int mainsize=sizeof(ar)/sizeof(ar[0]);
+    printf("the size of aptr %d\n",sizeof(aptr));
+    printf("size of array in ?%d, size of an element of array %d \n",sizeof(ar),sizeof(ar[0]));
+    printf("size of array in number of elements %d , size of an integer %d \n",sizeof(ar)/sizeof(ar[0]),sizeof(int));
+printf("average is %f\n",foo(ar,mainsize));
+printf("mainsize in main %d\n",mainsize);
+for (i=0;i<mainsize;i++)
+   {
+printf("average is %d\n",ar[i]);
+   }
+   printf("!!!!!%d\n",ar[20]);
+    printf("?!!!!%d\n",*(aptr+20));
+     printf("?!!!!%d\n",aptr[20]);
     return 0;
+}
+float foo(int a[],int size)
+{
+   float sum=0;
+   int i;
+
+   float result=0;
+
+
+   for (i=0;i<size;i++)
+   {
+       sum+=a[i];
+   }
+   result=sum/i;
+   printf(" i%d\n",i);
+    for (i=0;i<size;i++)
+   {
+       a[i]=-1;
+   }
+
+   return result;
 }
